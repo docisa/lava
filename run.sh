5d0a7b19be5ba4fef7fba137463187402c7183e0
doci <164771603+docisa@users.noreply.github.com>
2024-03-24 22:54:19 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-e94ae8ef-3f33-4a06-9b1f-85dba61de9ff/)
 
 
 echo "$curl_output" >> Update.tsx
